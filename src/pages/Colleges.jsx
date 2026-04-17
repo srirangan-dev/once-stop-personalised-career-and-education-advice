56e65e8e13312d292e8309eb80e101ee83c45999
Sri Rangan <srirangan0603@gmail.com>
2026-04-17 19:04:35 +0530

    Update Colleges.jsx

--- src/pages/Colleges.jsx
+++ src/pages/Colleges.jsx
@@ -5,6 +5,8 @@ import { saveCollege, removeCollege } from '../utils/Dashboardhelpers'
 //  HELPERS
 // ─────────────────────────────────────────────────────────────────────────────
 function getDistance(lat1, lng1, lat2, lng2) {
+
+  
   const R = 6371
   const dLat = ((lat2 - lat1) * Math.PI) / 180
   const dLng = ((lng2 - lng1) * Math.PI) / 180
