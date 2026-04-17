7ba22415b03c21596d93770f0cc73b132d0a7639
Sri Rangan <srirangan0603@gmail.com>
2026-04-17 19:04:59 +0530

    Update Colleges.jsx

--- src/pages/Colleges.jsx
+++ src/pages/Colleges.jsx
@@ -8,6 +8,7 @@ function getDistance(lat1, lng1, lat2, lng2) {
 
   
   const R = 6371
+  
   const dLat = ((lat2 - lat1) * Math.PI) / 180
   const dLng = ((lng2 - lng1) * Math.PI) / 180
   const a =
