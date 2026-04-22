ac85ca8f75f9ce2b3eac73a2ba2045ea6208e74a
Sri Rangan <srirangan0603@gmail.com>
2026-04-22 18:44:36 +0530

    Update Colleges.jsx

--- src/pages/Colleges.jsx
+++ src/pages/Colleges.jsx
@@ -9,8 +9,10 @@ function getDistance(lat1, lng1, lat2, lng2) {
   
   const R = 6371
   
+  
   const dLat = ((lat2 - lat1) * Math.PI) / 180
   const dLng = ((lng2 - lng1) * Math.PI) / 180
+  
   const a =
     Math.sin(dLat / 2) ** 2 +
     Math.cos((lat1 * Math.PI) / 180) * Math.cos((lat2 * Math.PI) / 180) *
