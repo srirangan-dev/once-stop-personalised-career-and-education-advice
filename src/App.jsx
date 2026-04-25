1dae37e96155f19416d1adf09eb8c1b03274f4aa
Sri Rangan <srirangan0603@gmail.com>
2026-04-25 19:00:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,8 @@ export default function App() {
             
 
             {/* ── Protected Routes ── */}
+
+            
             <Route path="/quiz" element={
               <ProtectedRoute><Quiz /></ProtectedRoute>
             } />
