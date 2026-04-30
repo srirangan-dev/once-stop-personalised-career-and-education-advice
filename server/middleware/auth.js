ef6743a07a64b103f8f536ef6445042a25005a22
Sri Rangan <srirangan0603@gmail.com>
2026-04-30 11:13:25 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -7,6 +7,8 @@ module.exports = function (req, res, next) {
         return res.status(401).json({ msg: 'No token, authorization denied' });
     }
 
+    
+
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded.userId;
